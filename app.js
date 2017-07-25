77d6fa471a81b72f95efa88fad5c808bb51d90a5
Crystal <crystal.r.mcgowan@gmail.com>
2017-07-25 18:51:52 -0400

    Data pulling OK, needs styling

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const data = require("data.js")
 const express = require("express")
 const app = express()
+const mustacheExpress = require("mustache-express")
+const path = require("path")
+app.use(express.static("public"))
+
+app.engine("mustache", mustacheExpress())
+app.set("views", "./views")
+app.set("view engine", "mustache")
+
+app.get("/", (req, res) => {
+  res.render("todo", data)
+})
+
+app.get("/people", (req, res) => {
+  res.render("people", data)
+})
 
 app.listen(3000, () => {
   console.log("listening")
